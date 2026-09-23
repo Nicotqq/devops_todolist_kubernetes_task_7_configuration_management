fc78fd1ea703d91365f68e91ea16ec742a1c1632
Mykola Hakh <kolyagakh@gmail.com>
2026-09-23 17:15:20 +0300

    Lesson complete

--- src/todolist/settings.py
+++ src/todolist/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
